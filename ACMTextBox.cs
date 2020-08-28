4dbc17c167091e6f30588424cc0c6ddab64f9ff1
yinglan chi <elainechi@yinglans-MacBook-Air.local>
2020-08-28 00:23:44 -0700

    add files

---
+++ ACMTextBox.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ACM.Presentation.Controls.Text
+{
+    public class ACMTextBox : TextBox
+    {
+        #region Static Constructor
+        static ACMTextBox()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(ACMTextBox), new FrameworkPropertyMetadata(typeof(ACMTextBox)));
+        }
+
+        #endregion
+
+        #region Properties
+
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(string), typeof(ACMTextBox), new FrameworkPropertyMetadata(String.Empty));
+        public static readonly DependencyProperty CaptionPlacementProperty = DependencyProperty.Register("CaptionPlacement", typeof(captionPlacement), typeof(ACMTextBox), new PropertyMetadata(captionPlacement.Right));
+        public static readonly DependencyProperty FontColorProperty = DependencyProperty.Register("FontColor", typeof(fontColor), typeof(ACMTextBox), new FrameworkPropertyMetadata(fontColor.Gray));
+        public static readonly DependencyProperty TextBoxTypeProperty = DependencyProperty.Register("TextBoxType", typeof(ACMTextBoxType), typeof(ACMTextBox), new FrameworkPropertyMetadata(ACMTextBoxType.DefaultMode));
+        public static readonly DependencyProperty IsTypingStartProperty = DependencyProperty.Register("IsTypingStart", typeof(bool), typeof(ACMTextBox), new PropertyMetadata(false, OnTypingStartPropertyChanged));
+        public static readonly DependencyProperty IsTypingCompletedProperty = DependencyProperty.Register("IsTypingCompleted", typeof(bool), typeof(ACMTextBox), new PropertyMetadata(false, OnTypingCompletedChanged));
+        public static readonly RoutedEvent TypingCompletedRoutedEvent = EventManager.RegisterRoutedEvent("TypingCompleted", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ACMTextBox));
+
+        #endregion
+
+        #region Getters and Setters
+
+        #region Getter and Setter for 'Caption'
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        #endregion
+
+        #region Getter and Setter for 'CaptionPlacement'
+
+        public captionPlacement CaptionPlacement
+        {
+            get { return (captionPlacement)GetValue(CaptionPlacementProperty); }
+            set { SetValue(CaptionPlacementProperty, value); }
+        }
+
+        #endregion
+
+        #region Getter and Setter for 'FontColor'
+
+        public fontColor FontColor
+        {
+            get { return (fontColor)GetValue(FontColorProperty); }
+            set { SetValue(FontColorProperty, value); }
+        }
+
+        #endregion
+
+        #region Getter and Setter for 'TextBoxType'
+
+        public ACMTextBoxType TextBoxType
+        {
+            get { return (ACMTextBoxType)GetValue(TextBoxTypeProperty); }
+            set { SetValue(TextBoxTypeProperty, value); }
+        }
+
+        #endregion
+
+        #region Getter and Setter for 'IsTypingStart'
+       
+        public bool IsTypingStart
+        {
+            get { return (bool)GetValue(IsTypingStartProperty); }
+            set { SetValue(IsTypingStartProperty, value); }
+        }
+
+        #endregion
+
+        #region Getter and Setter for 'IsTypingCompleted'
+        public bool IsTypingCompleted
+        {
+            get { return (bool)GetValue(IsTypingCompletedProperty); }
+            set { SetValue(IsTypingCompletedProperty, value); }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region ENUM variables
+
+        #region ACMTextBoxType
+        /// <summary>
+        /// The Mode that the textbox is in
+        /// </summary>
+        public enum ACMTextBoxType
+        {
+            DefaultMode,        // Default mode
+            TypingMode,         // When user starts typing in the textbox
+        }
+
+        #endregion
+
+        #region CaptionPlacement
+        /// <summary>
+        /// Where the title is placed (depending on the mode of the textbox
+        /// </summary>
+        public enum captionPlacement
+        {
+            Left,
+            Top,
+            Right,
+            Bottom
+        }
+
+        #endregion
+
+        #region fontColor
+
+        public enum fontColor
+        {
+            Green,
+            Gray
+        }
+
+        #endregion
+
+        #endregion
+
+        /// <summary>
+        /// When the user started typing 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="args"></param>
+        static void OnTypingStartPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            ACMTextBox @this = obj as ACMTextBox;
+
+            bool newValue = (bool)args.NewValue;
+            if (newValue != true) return;
+
+            @this.IsTypingCompleted = false;
+            @this.FontColor = fontColor.Green;
+            @this.CaptionPlacement = captionPlacement.Top;
+        }
+
+
+        /// <summary>
+        /// When the user finished typing
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="args"></param>
+        static void OnTypingCompletedChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            ACMTextBox @this = obj as ACMTextBox;
+
+            bool newValue = (bool)args.NewValue;
+            if (newValue != true) return;
+
+            @this.RaiseTypingCompletedEvent();
+        }
+
+        private void RaiseTypingCompletedEvent()
+        {
+            RoutedEventArgs args = new RoutedEventArgs();
+            args.RoutedEvent = TypingCompletedRoutedEvent;
+            RaiseEvent(args);
+        }
+    }
+
+    public class converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = value as string;
+
+            if (text != null)
+            {
+                return text.Length > 0;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ UserControlTextBox.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace ACM.Presentation.Views.Examples{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+public partial class UserControlTextBox : UserControl
+    {
+        public UserControlTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            ACM.Presentation.Controls.Text.ACMTextBox tbox = sender as ACM.Presentation.Controls.Text.ACMTextBox;
+            tbox.IsTypingStart = true;
+            if (e.Key == Key.Return)
+            {
+                tbox.IsTypingCompleted = true;
+            }
+        }
+    }
+}
